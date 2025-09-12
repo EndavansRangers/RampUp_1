a331a363dccda4f8e85900ce868dd22bbe4ac45e
Super user <Super user@TeamCity>
2025-09-12 00:41:14 +0000

    runners of 'Tunefy' build configuration were updated (TeamCity change in 'Tunefy' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -93,11 +93,12 @@ object Tunefy : BuildType({
                 HOST_FRONTEND="${'$'}CHECKOUT/frontend"
                 
                 tar -C "${'$'}HOST_FRONTEND" -cf - . \
-                | docker run --rm -i -w /app -e CI=true node:18-alpine sh -lc '
-                  set -eu
-                  tar -xf - -C /app
-                  npm ci
-                  npm run build
+                | docker run --rm -i -w /app \
+                  node:18-alpine sh -lc '
+                    set -e
+                    tar -xf - -C /app
+                    npm ci --no-audit --no-fund
+                    CI= npm run build
                 '
             """.trimIndent()
         }
