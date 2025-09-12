d32d8b9ea39be78a17155de897108b4206196582
Super user <Super user@TeamCity>
2025-09-12 01:50:07 +0000

    parameters of 'Tunefy' build configuration were updated (TeamCity change in 'Tunefy' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -36,7 +36,7 @@ object Tunefy : BuildType({
 
     params {
         param("env.DOCKER_REGISTRY", "10.20.0.150:5000")
-        param("GitVersion.SemVer", "1.0.0")
+        param("GitVersion.SemVer", "1.1.0")
     }
 
     vcs {
