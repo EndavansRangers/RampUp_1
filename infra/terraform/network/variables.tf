9bfcc889f968d96298662d40ea76905101f84044
dcifuentesg <dcifuentesg@unal.edu.co>
2025-09-23 10:25:23 -0500

    Add Terraform variables for network configuration

---
+++ infra/terraform/network/variables.tf
@@ -0,0 +1,24 @@
+variable "project" { type = string }
+variable "env"     { type = string } # "dev" | "prod"
+variable "region"  { type = string }
+
+variable "azs" {
+  description = "AZs to use"
+  type        = list(string)
+}
+
+variable "vpc_cidr" {
+  description = "VPC CIDR"
+  type        = string
+}
+
+variable "nat_per_az" {
+  description = "true = NAT for each AZ (HA), false = only one NAT"
+  type        = bool
+  default     = false
+}
+
+variable "tags" {
+  type = map(string)
+  default = {}
+}
