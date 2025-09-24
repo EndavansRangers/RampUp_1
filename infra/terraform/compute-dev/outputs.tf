cd1158bb486031f0e2942972fe6c29212fa8086f
dcifuentesg <dcifuentesg@unal.edu.co>
2025-09-24 15:19:40 -0500

    Add output variables for bastion, control plane, and instance IPs in Terraform

---
+++ infra/terraform/compute-dev/outputs.tf
@@ -0,0 +1,6 @@
+output "bastion_public_ip"         { value = aws_instance.bastion.public_ip }
+output "control_plane_endpoint"    { value = aws_lb.cp.dns_name }
+output "cp_asg_name"               { value = aws_autoscaling_group.cp.name }
+output "wk_asg_name"               { value = aws_autoscaling_group.wk.name }
+output "teamcity_private_ip"       { value = aws_instance.teamcity.private_ip }
+output "octopus_private_ip"        { value = aws_instance.octopus.private_ip }
