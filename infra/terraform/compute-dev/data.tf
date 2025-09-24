591c454a14c26cd83efe092bd7ee40bd1357753a
dcifuentesg <dcifuentesg@unal.edu.co>
2025-09-24 15:19:29 -0500

    Add data source for Ubuntu 22.04 AMI in Terraform configuration

---
+++ infra/terraform/compute-dev/data.tf
@@ -0,0 +1,12 @@
+locals {
+  name        = "${var.project}-${var.env}"
+  common_tags = merge({ Project = var.project, Env = var.env }, var.tags)
+}
+
+# Ubuntu 22.04 LTS (Canonical)
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+  filter { name = "name"; values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] }
+  filter { name = "virtualization-type"; values = ["hvm"] }
+}
