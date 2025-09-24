b860a257a2bcdfe228aafa2602928542ec9a95a4
dcifuentesg <dcifuentesg@unal.edu.co>
2025-09-24 15:31:45 -0500

    Format filters for AWS AMI data source for improved readability in Terraform configuration

--- infra/terraform/compute-dev/data.tf
+++ infra/terraform/compute-dev/data.tf
@@ -7,6 +7,15 @@ locals {
 data "aws_ami" "ubuntu" {
   most_recent = true
   owners      = ["099720109477"]
-  filter { name = "name"; values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"] }
-  filter { name = "virtualization-type"; values = ["hvm"] }
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
+
