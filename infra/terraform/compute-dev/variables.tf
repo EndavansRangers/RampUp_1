187ada9e79bf233c9548bcbe60e827367188d97a
dcifuentesg <dcifuentesg@unal.edu.co>
2025-09-24 15:30:58 -0500

    Refactor variable definitions for improved readability in Terraform configuration

--- infra/terraform/compute-dev/variables.tf
+++ infra/terraform/compute-dev/variables.tf
@@ -1,6 +1,12 @@
 variable "project" { type = string }
-variable "region"  { type = string; default = "us-east-1" }
-variable "env"     { type = string; default = "dev" }
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+variable "env" { 
+    type = string
+    default = "dev" 
+}
 variable "key_name" {
   description = "Key pair name for SSH"
   type        = string
@@ -15,10 +21,22 @@ variable "private_subnet_ids"  { type = list(string) }  # Dev
 variable "nodes_instance_profile_name" { type = string }
 
 # Instance types
-variable "cp_instance_type" { type = string; default = "c7i-flex.large" }
-variable "wk_instance_type" { type = string; default = "t3.small" }
-variable "tc_instance_type" { type = string; default = "c7i-flex.large" }
-variable "oc_instance_type" { type = string; default = "c7i-flex.large" }
+variable "cp_instance_type" {
+    type = string
+    default = "c7i-flex.large"
+}
+variable "wk_instance_type" {
+    type = string
+    default = "t3.small" 
+}
+variable "tc_instance_type" {
+    type = string
+    default = "c7i-flex.large"
+}
+variable "oc_instance_type" {
+    type = string
+    default = "c7i-flex.large"
+}
 
 # IP for bastion SG (CIDR /32)
 variable "allowed_ssh_cidr" { type = string } 
