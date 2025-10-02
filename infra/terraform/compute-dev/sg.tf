adccddd3aca7151f83c5b1c3f4dc587aa1d0f1ef
dcifuentesg <dcifuentesg@unal.edu.co>
2025-10-02 15:21:36 -0500

    fix: Update Octopus security group rules to allow self-referencing for UI and Tentacle Polling

--- infra/terraform/compute-dev/sg.tf
+++ infra/terraform/compute-dev/sg.tf
@@ -149,13 +149,15 @@ resource "aws_security_group" "cicd" {
     to_port         = 8080
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id, aws_security_group.cp.id]
-  } # Octopus UI (from bastion and CP)
+    self            = true
+  } # Octopus UI (from bastion, CP, and other CICD instances)
   ingress {
     from_port       = 10943
     to_port         = 10943
     protocol        = "tcp"
     security_groups = [aws_security_group.bastion.id, aws_security_group.cp.id]
-  } # Octopus Tentacle Polling (from bastion and CP)
+    self            = true
+  } # Octopus Tentacle Polling (from bastion, CP, and other CICD instances)
 
   egress {
     from_port   = 0
