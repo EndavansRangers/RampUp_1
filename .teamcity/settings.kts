2b5afc26cbb6455d2a58a2465d5ae4754af23bd7
admin <admin@TeamCity>
2025-10-15 16:14:08 +0000

    TeamCity change in 'Tunefy' project: runners of 'Tunefy' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -89,7 +89,7 @@ object Tunefy : BuildType({
                 
                 docker build -t "${'$'}FRONT" \
                   --build-arg REACT_APP_BACKEND_URL="/api" \
-                  --build-arg REACT_APP_FRONTEND_URL="k8s-tunefyde-tunefyfr-798b6aea21-6385752.us-east-1.elb.amazonaws.com" \
+                  #--build-arg REACT_APP_FRONTEND_URL="k8s-tunefyde-tunefyfr-798b6aea21-6385752.us-east-1.elb.amazonaws.com" \
                   --build-arg REACT_APP_GOOGLE_KEY="AIzaSyAAL1GtGXpN3NEgcbRUqQvEzNaRMk740uM" \
                   -f frontend/Dockerfile frontend
                   
