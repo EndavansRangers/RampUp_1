be356e58070a0ca5bc10298cbd1fc02734d9f7ce
admin <admin@TeamCity>
2025-10-15 01:47:29 +0000

    TeamCity change in 'Tunefy' project: Versioned settings configuration updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -38,8 +38,8 @@ object Tunefy : BuildType({
         param("env.COLOR_NEXT", "blue")
         param("env.DOCKER_REGISTRY", "638325785916.dkr.ecr.us-east-1.amazonaws.com")
         param("env.AWS_DEFAULT_REGION", "us-east-1")
-        param("env.OCTO_URL", "http://10.20.63.199:8080")
         password("env.OCTO_API_KEY", "credentialsJSON:223c7874-6618-4f07-b353-809e4ca77e0e")
+        param("env.OCTO_URL", "http://10.20.63.199:8080")
     }
 
     vcs {
