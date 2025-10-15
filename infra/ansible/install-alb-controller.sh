3e0ebc6ae7e22fb237767b7f52a07e10a6c57ab4
dcifuentesg <dcifuentesg@unal.edu.co>
2025-10-15 10:51:58 -0500

    test

--- infra/ansible/install-alb-controller.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-helm upgrade --install aws-load-balancer-controller eks/aws-load-balancer-controller \
-  -n kube-system --create-namespace \
-  --set clusterName=tunefy-dev \
-  --set region=us-east-1 \
-  --set vpcId=vpc-020517f941f39faeb \
-  --set serviceAccount.create=true \
-  --set image.repository=amazon/aws-load-balancer-controller
--- infra/ansible/install-external-dns.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-helm upgrade --install external-dns bitnami/external-dns \
-  -n kube-system \
-  --set provider=aws \
-  --set policy=sync \
-  --set registry=txt \
-  --set txtOwnerId=tunefy-dev \
-  --set domainFilters={endavarangers.com}
